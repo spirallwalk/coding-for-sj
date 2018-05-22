51d1f7eb7292c6babc90a7ba2887cb5fa5fc914e
spirallwlak <endlesefort@gmail.com>
2018-05-22 17:42:50 +0900

    add 02-data-type-1.py

---
+++ 02-date-type-1.py
@@ -0,0 +1,23 @@
+#정수 int = integer
+print(1 + 1)
+#소수 float
+print(1.5 + 2.1)
+#문자 str = string
+print("hello")
+
+#문자열 더하기 "?"<-따움표 안에 넣어야 문자라고 인식한다.
+print("hello" + "1")
+
+# 정수 소수 간 변경 (//),(int) = 숫자를 정수로 표시 ,float()는 소수점 까지 표시
+print(4 // 2)
+print(int(4 / 2))
+print(float(2))
+
+#문자열 숫자 간 변경
+print("1")
+print(int("1"))
+print(int("1.5"))
+
+#print(int(1.5)) <- 1.5를 정수로 표시하라는 명령을 내렸을때 어떤 값이 나올까? 정답은 2번 오류가 난다.
+# 1)1
+# 2) error
